e97ec234b98c3b1daceb96da43a11a267d303646
Noble Obioma <nobioma1@gmail.com>
2020-04-01 21:42:08 +0100

    Add Route to App Component
    - Setup Routing Filter, CardOwners components
    - Fix path to to use alias
    - Add globalStyle to app

---
+++ client/src/components/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { AppContainer } from './app.style';
+import CarOwners from 'components/CarOwners';
+import Filters from 'components/Filters';
+import { GlobalStyle } from '../../shared/GlobalStyle';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Switch>
+        <AppContainer>
+          <Route exact path="/" component={Filters} />
+          <Route path="/filter/:filterId" component={CarOwners} />
+        </AppContainer>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ client/src/components/App/app.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  margin: 0 auto;
+  max-width: 780px;
+  padding: 10px;
+`;

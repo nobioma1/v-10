83a4f5a770d839bbc58ad3a196b14b41c6ad1402
Noble Obioma <nobioma1@gmail.com>
2020-04-01 10:04:45 +0100

    Create router and add to app as middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,14 @@
 const cors = require('cors');
 const express = require('express');
 
+const apiRouter = require('./routes');
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', apiRouter);
+
 
 app.listen(PORT, () => console.log(`live @PORT: ${PORT} 🚀`));
---
+++ server/src/routes/index.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+
+const apiRouter = Router();
+
+module.exports = apiRouter;

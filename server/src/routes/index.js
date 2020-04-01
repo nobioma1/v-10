00591b5e3e48f382286bd8903440cf2c56fd1134
Noble Obioma <nobioma1@gmail.com>
2020-04-01 10:38:11 +0100

    Add route to return filtered car owners by passing query param filter id.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,32 @@
 const { Router } = require('express');
 
-const { getFilters } = require('../models');
+const { getFilters, getFilterById, getCarOwners } = require('../models');
+const filterCarOwners = require('../utils/filterCarOwners');
+
 const apiRouter = Router();
 
 apiRouter.get('/filters', async (req, res, next) => {
   try {
     const filters = await getFilters();
     res.status(200).json({ filters });
+  } catch (error) {
+    next(error);
+  }
+});
+
+apiRouter.get('/car_owners', async (req, res, next) => {
+  try {
+    if (req.query.filter) {
+      const filter = await getFilterById(parseInt(req.query.filter, 10));
+
+      if (filter) {
+        const carOwners = await getCarOwners();
+        const filteredCarOwners = filterCarOwners(carOwners, filter);
+        return res.status(200).json({ filteredCarOwners });
+      }
+      throw new Error('Filter with ID does not exist');
+    }
+    throw new Error('Provide the filter ID');
   } catch (error) {
     next(error);
   }
